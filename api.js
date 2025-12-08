870496be4ca1df392f9852275700515206d26acc
luks2211 <ethanmanapil@gmail.com>
2025-12-08 20:03:20 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'html')));
-app.use(express.static(path.join(__dirname, 'Css')));
-
-// Your API routes here
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-module.exports = app;
+{
+  "version": 2,
+  "builds": [
+    {
+      "src": "api.js",
+      "use": "@vercel/node"
+    }
+  ],
+  "routes": [
+    {
+      "src": "/(.*)",
+      "dest": "api.js"
+    }
+  ]
+}
